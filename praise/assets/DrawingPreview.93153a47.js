94c73f800a92461be5cee22914a2002f8b0f1caa
lism <545314690@qq.com>
2022-10-31 22:40:31 +0800

    add icon

---
+++ praise/assets/DrawingPreview.93153a47.js
@@ -0,0 +1,1 @@
+import"./index.0c988825.js";import{aA as r}from"./index.0c988825.js";export{r as default};
--- praise/assets/DrawingPreview.cf019e54.js
+++
@@ -1,1 +0,0 @@
-import"./index.6f53455b.js";import{aA as r}from"./index.6f53455b.js";export{r as default};
