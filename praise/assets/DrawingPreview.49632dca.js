336aa7ecfde76b4cb39e81c848d5c22bcf8de9ad
lism <545314690@qq.com>
2022-11-06 07:58:33 +0800

    add playMode

--- praise/assets/DrawingPreview.49632dca.js
+++
@@ -1,1 +0,0 @@
-import"./index.442b49b6.js";import{aA as r}from"./index.442b49b6.js";export{r as default};
---
+++ praise/assets/DrawingPreview.bd4ea90a.js
@@ -0,0 +1,1 @@
+import"./index.216fef64.js";import{aA as r}from"./index.216fef64.js";export{r as default};
